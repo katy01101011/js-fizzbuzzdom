3feff1036e9dc8f14925d901529506cac3289f2a
katy01101011 <katy.91560@gmail.com>
2022-04-12 09:21:00 +0200

    Bonus 2

--- Bonus 2/js/script.js
+++ Bonus 2/js/script.js
@@ -14,19 +14,19 @@ let fizzBuzz = i;
 if (i % 3 === 0 && i % 5 === 0) {
     fizzBuzz = 'FizzBuzz';
     console.log(fizzBuzz);
-    fizzBox.innerHTML += `<div class="box">${fizzBuzz}</div>`;
+    fizzBox.innerHTML += `<div class="box fizz-buzz">${fizzBuzz}</div>`;
 } else if (i % 3 === 0) {
     fizzBuzz = 'Fizz';
     console.log(fizzBuzz);
-    fizzBox.innerHTML += `<div class="box">${fizzBuzz}</div>`;
+    fizzBox.innerHTML += `<div class="box fizz">${fizzBuzz}</div>`;
 } else if (i % 5 === 0) {
     fizzBuzz = 'Buzz';
     console.log(fizzBuzz);
-    fizzBox.innerHTML += `<div class="box">${fizzBuzz}</div>`;
+    fizzBox.innerHTML += `<div class="box buzz">${fizzBuzz}</div>`;
 } else {
     fizzBuzz = `${fizzBuzz}`;
     console.log(fizzBuzz);
-    fizzBox.innerHTML += `<div class="box">${fizzBuzz}</div>`;
+    fizzBox.innerHTML += `<div class="box f-b">${fizzBuzz}</div>`;
 }
 }
 
